3f6c148b83e8bc797d7c6ecb54eb437e0ec24adb
owunmiv <wunmieeplus@gmail.com>
2025-11-19 16:37:05 +0100

    Initial Commit

---
+++ Countti/CounttiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounttiApp.swift
+//  Countti
+//
+//  Created by Omowunmi on 19/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CounttiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
