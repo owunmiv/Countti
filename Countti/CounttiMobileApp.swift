1f3e1498e667e2ee8ea71d1622dd6f09523be245
owunmiv <wunmieeplus@gmail.com>
2025-12-27 21:24:39 +0100

    Implemented the add event page and the fields

--- Countti/CounttiMobileApp.swift
+++ Countti/CounttiMobileApp.swift
@@ -6,12 +6,18 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
-struct CounttiApp: App {
+struct CounttiMobileApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Event.self)
+    }
+    
+    init() {
+        print(URL.applicationSupportDirectory.path(percentEncoded: false))
     }
 }
